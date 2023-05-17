c621b900701a90c5f8d4c7394e0b64f459d520d5
sporeball <sporeballdev@gmail.com>
2023-05-16 17:02:06 -0700

    mui and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,30 @@ import { useState } from 'react'
 import Update from '@/components/update'
 import './App.scss'
 
+import {
+  Button,
+  CssBaseline,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField
+} from '@mui/material';
+
 console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
-    <div className='App'>
-      <h1>Electron + Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <>
+      <CssBaseline />
+      <div className='App'>
+        <h1 className="text-3xl font-bold mb-4">PLUR</h1>
+        <p className="mb-4">What are you going to do today?</p>
       </div>
-
-      <Update />
-    </div>
+    </>
   )
 }
+
+// <Update />
 
 export default App
